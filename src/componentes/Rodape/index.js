d5fd153fe28c22d69d8dcaae9caa0deb05ef1003
Elson Soares <113075447+ElsonSoares202@users.noreply.github.com>
2025-05-01 18:38:29 -0300

    Update index.js

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -26,7 +26,7 @@ const Rodape = () => {
         </section>
         <section>
             <p>
-                Desenvolvido por Alura.
+                Desenvolvido por Elson Soares.
             </p>
         </section>
     </footer>)
